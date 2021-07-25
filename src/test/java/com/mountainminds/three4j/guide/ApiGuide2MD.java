d77ad99b61ed3803aa14494a85e38488344aea25
Marc R. Hoffmann <hoffmann@mountainminds.com>
2021-07-25 19:15:06 +0200

    Update and verify API guide in README.md

--- src/test/java/com/mountainminds/three4j/guide/ApiGuide2MD.java
+++ src/test/java/com/mountainminds/three4j/guide/ApiGuide2MD.java
@@ -1,46 +1,90 @@
+/*******************************************************************************
+ * Copyright (c) 2021 Mountainminds GmbH & Co. KG
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ * SPDX-License-Identifier: MIT
+ *******************************************************************************/
 package com.mountainminds.three4j.guide;
 
-import static java.lang.System.out;
 import static java.nio.charset.StandardCharsets.UTF_8;
 
 import java.io.IOException;
+import java.io.PrintStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.List;
+import java.util.function.Consumer;
 
 /**
  * Extracts Markdown documentation from the example code.
  */
 public class ApiGuide2MD {
 
-	private static final String SOURCE = "src/test/java/com/mountainminds/three4j/guide/ApiGuide.java";
+	static final Path README = Path.of("README.md");
+
+	static final Path SOURCE = Path.of("src/test/java/com/mountainminds/three4j/guide/ApiGuide.java");
+
+	static final String API_CHAPTER = "## API Usage Guide";
 
 	public static void main(String[] args) throws IOException {
+		List<String> original = Files.readAllLines(README, UTF_8);
+		try (var out = new PrintStream(Files.newOutputStream(README), true, UTF_8)) {
+			writeUpdatedAPIGuide(original, out::println);
+		}
+	}
+
+	public static void writeUpdatedAPIGuide(List<String> original, Consumer<String> out) throws IOException {
+		boolean keep = true;
+		for (var line : original) {
+			if (API_CHAPTER.equals(line)) {
+				out.accept(line);
+				out.accept("");
+				writeAPIGuide(out);
+				out.accept("");
+				keep = false;
+			} else if (line.startsWith("## ")) {
+				keep = true;
+			}
+			if (keep) {
+				out.accept(line);
+			}
+		}
+	}
+
+	public static void writeAPIGuide(Consumer<String> out) throws IOException {
 
 		boolean code = false;
 
-		for (String line : Files.readAllLines(Path.of(SOURCE), UTF_8)) {
+		for (String line : Files.readAllLines(SOURCE, UTF_8)) {
 			line = line.strip();
 			if (line.contains("// HIDE")) {
 				continue;
 			}
 			if (line.equals("// <CODE>")) {
-				out.println();
-				out.println("```java");
+				out.accept("");
+				out.accept("```java");
 				code = true;
 				continue;
 			}
 			if (line.equals("// </CODE>")) {
-				out.println("```");
-				out.println();
+				out.accept("```");
+				out.accept("");
 				code = false;
 				continue;
 			}
 			if (line.startsWith("//")) {
-				out.println(line.substring(2).strip());
+				out.accept(line.substring(2).strip());
 				continue;
 			}
 			if (code) {
-				out.println(line);
+				out.accept(line);
 			}
 		}
 	}
---
+++ src/test/java/com/mountainminds/three4j/guide/ApiGuideTest.java
@@ -0,0 +1,35 @@
+/*******************************************************************************
+ * Copyright (c) 2021 Mountainminds GmbH & Co. KG
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ * SPDX-License-Identifier: MIT
+ *******************************************************************************/
+package com.mountainminds.three4j.guide;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.Test;
+
+public class ApiGuideTest {
+
+	@Test
+	void readme_should_contain_latest_apiguide_source() throws IOException {
+		var actual = Files.readAllLines(ApiGuide2MD.README, UTF_8);
+		var expected = new ArrayList<String>();
+		ApiGuide2MD.writeUpdatedAPIGuide(actual, expected::add);
+		assertEquals(expected, actual);
+	}
+
+}
