df7cfd9ff0ed44591b4272241204555aaa545385
Marc R. Hoffmann <hoffmann@mountainminds.com>
2021-07-27 16:57:41 +0200

    Provide Three4J as Java module

--- src/test/java/com/mountainminds/three4j/ByteArrayValueTest.java
+++ src/test/java/com/mountainminds/three4j/ByteArrayValueTest.java
@@ -31,53 +31,53 @@ public class ByteArrayValueTest {
 	}
 
 	@Test
-	void should_throw_IllegalArgumentException_for_wrong_array_length() {
+	public void should_throw_IllegalArgumentException_for_wrong_array_length() {
 		var e = assertThrows(IllegalArgumentException.class, () -> new Subject(new byte[5]));
 		assertEquals("Illegal value size: 5 bytes", e.getMessage());
 	}
 
 	@Test
-	void getValue_should_return_value() {
+	public void getValue_should_return_value() {
 		var s = new Subject(new byte[] { 1, 2, 3, 4 });
 		assertArrayEquals(new byte[] { 1, 2, 3, 4 }, s.getValue());
 	}
 
 	@Test
-	void hashCode_should_be_different_for_different_values() {
+	public void hashCode_should_be_different_for_different_values() {
 		var s1 = new Subject(new byte[] { 1, 2, 3, 4 });
 		var s2 = new Subject(new byte[] { 1, 4, 3, 4 });
 		assertNotEquals(s1.hashCode(), s2.hashCode());
 	}
 
 	@Test
-	void hashCode_should_be_same_for_same_values() {
+	public void hashCode_should_be_same_for_same_values() {
 		var s1 = new Subject(new byte[] { 1, 2, 3, 4 });
 		var s2 = new Subject(new byte[] { 1, 2, 3, 4 });
 		assertEquals(s1.hashCode(), s2.hashCode());
 	}
 
 	@Test
-	void equals_should_be_true_for_equal_values_and_types() {
+	public void equals_should_be_true_for_equal_values_and_types() {
 		var s1 = new Subject(new byte[] { 1, 2, 3, 4 });
 		var s2 = new Subject(new byte[] { 1, 2, 3, 4 });
 		assertTrue(s1.equals(s2));
 	}
 
 	@Test
-	void equals_should_be_false_for_null() {
+	public void equals_should_be_false_for_null() {
 		var s1 = new Subject(new byte[] { 1, 2, 3, 4 });
 		assertFalse(s1.equals(null));
 	}
 
 	@Test
-	void equals_should_be_false_for_different_values() {
+	public void equals_should_be_false_for_different_values() {
 		var s1 = new Subject(new byte[] { 1, 2, 3, 4 });
 		var s2 = new Subject(new byte[] { 1, 2, 5, 4 });
 		assertFalse(s1.equals(s2));
 	}
 
 	@Test
-	void equals_should_be_false_for_different_types() {
+	public void equals_should_be_false_for_different_types() {
 		var s1 = new Subject(new byte[] { 1, 2, 3, 4 });
 		var s2 = new Subject(new byte[] { 1, 2, 3, 4 }) {
 		};
@@ -85,7 +85,7 @@ public class ByteArrayValueTest {
 	}
 
 	@Test
-	void toString_should_print_hex_value() {
+	public void toString_should_print_hex_value() {
 		var s = new Subject(new byte[] { 0x0a, 0x12, 0x34, 0x56 });
 		assertEquals("Subject[0a123456]", s.toString());
 	}
--- src/test/java/com/mountainminds/three4j/HttpSupportTest.java
+++ src/test/java/com/mountainminds/three4j/HttpSupportTest.java
@@ -30,38 +30,38 @@ import com.mountainminds.three4j.HttpSupport.UrlParams;
 public class HttpSupportTest {
 
 	@Test
-	void UNKNOWN_RESPONSE_should_always_throw_a_exception() {
+	public void UNKNOWN_RESPONSE_should_always_throw_a_exception() {
 		var e = assertThrows(GatewayException.class, () -> UNKNOWN_RESPONSE.handle(123));
 		assertEquals(123, e.getStatus());
 		assertEquals("Unknown response (123)", e.getMessage());
 	}
 
 	@Test
-	void StatusHandler_ok_should_accept_200() throws GatewayException {
+	public void StatusHandler_ok_should_accept_200() throws GatewayException {
 		UNKNOWN_RESPONSE.ok().handle(200);
 	}
 
 	@Test
-	void StatusHandler_ok_should_not_handle_other_codes_than_200() {
+	public void StatusHandler_ok_should_not_handle_other_codes_than_200() {
 		assertThrows(GatewayException.class, () -> UNKNOWN_RESPONSE.ok().handle(201));
 	}
 
 	@Test
-	void StatusHandler_error_should_throw_specific_exception() {
+	public void StatusHandler_error_should_throw_specific_exception() {
 		var e = assertThrows(GatewayException.class, () -> UNKNOWN_RESPONSE.error(555, "test error").handle(555));
 		assertEquals(555, e.getStatus());
 		assertEquals("test error (555)", e.getMessage());
 	}
 
 	@Test
-	void StatusHandler_error_should_not_handle_other_codes() {
+	public void StatusHandler_error_should_not_handle_other_codes() {
 		var e = assertThrows(GatewayException.class, () -> UNKNOWN_RESPONSE.error(555, "error").handle(500));
 		assertEquals(500, e.getStatus());
 		assertEquals("Unknown response (500)", e.getMessage());
 	}
 
 	@Test
-	void blobBody_should_create_correct_multipart_content() throws IOException {
+	public void blobBody_should_create_correct_multipart_content() throws IOException {
 		var body = new String(blobBody("<content>".getBytes(US_ASCII)), US_ASCII);
 		assertEquals("--xZK2aOVCeCybl1bbgvCEas6n4cdntpzkpcLWA12SahAiBrDrkIBj3W2HMPghi3Bo\r\n" //
 				+ "Content-Disposition: form-data;name=\"blob\";filename=\"blob\"\r\n" //
@@ -71,14 +71,14 @@ public class HttpSupportTest {
 	}
 
 	@Test
-	void urlparams_should_encode_parameters() {
+	public void urlparams_should_encode_parameters() {
 		var params = new UrlParams();
 		params.add("a", "123").add("b", "Hello+World!");
 		assertEquals("a=123&b=Hello%2BWorld%21", params.toString());
 	}
 
 	@Test
-	void decodeUrlParams_should_decode_params() {
+	public void decodeUrlParams_should_decode_params() {
 		assertEquals(Map.of("a", "123", "b", "Hello World!"), decodeUrlParams("a=123&b=Hello+World%21"));
 	}
 
